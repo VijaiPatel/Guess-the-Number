1e44d08b0ddb194bfa487fe052b0f8fbfa8ae10f
VijaiPatel <48962282+VijaiPatel@users.noreply.github.com>
2023-06-08 10:28:19 +0100

    Add project files.

---
+++ Guess the Number/HigherorLower.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guess_the_Number
+{
+    class HigherorLower
+    {
+        public String whereisthenumber(int a, int b)// a will be the random number, b will be user input
+        {
+            // need to determine whether a is greater or lower than b
+            if (a==b)
+            {
+                return "You got it!";
+            }
+            else if (b>a)
+            {
+                return "Higher";
+            }
+            else
+            {
+                return "Lower";
+            }
+        }
+    }
+}
---
+++ Guess the Number/Program.cs
@@ -0,0 +1,54 @@
+﻿using Guess_the_Number;
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random randomiser = new Random();
+            HigherorLower guesser = new HigherorLower();
+            int therandomnumber = randomiser.Next(1, 100); // between 1-99
+            String userinput = "";
+            int usernmber = 0;
+            String responses = "";
+            bool iswin = false;
+            Console.WriteLine("guess a number between 1 and 99, you get 5 tries, I will tell you if you are higher or lower");
+            userinput = Console.ReadLine(); //reads what user gives
+            usernmber = Convert.ToInt32(userinput);
+            Console.WriteLine($"the random number is: {therandomnumber} the user input is {userinput} converted number is {usernmber}");
+            for (int i = 0; i<5; i++)
+            {
+                responses = guesser.whereisthenumber(therandomnumber, usernmber);
+                if (responses== "You got it!")
+                {
+                    Console.WriteLine($"Congrats you guessed the number which was {therandomnumber}");
+                    iswin = true;
+                    break;
+                }
+                else if (responses=="Higher")
+                {
+                    Console.WriteLine($"Your number {usernmber} is Higher than my number {therandomnumber}");
+                    Console.WriteLine("try again with another number");
+                    userinput = Console.ReadLine();
+                    usernmber = Convert.ToInt32(userinput);
+                }
+                else
+                {
+                    Console.WriteLine($"Your number {usernmber} is lower than my number {therandomnumber}");
+                    Console.WriteLine("try again with another number");
+                    userinput = Console.ReadLine();
+                    usernmber = Convert.ToInt32(userinput);
+                }
+            }
+            if (iswin==false) 
+            {
+                Console.WriteLine($"You loose! the number was {therandomnumber}");
+            }
+            
+            Console.ReadLine();
+
+        }
+    }
+}
